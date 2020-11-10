adcbf93df19cca8f5d02aea3cf78c577efd2defa
radagast666 <ecampos853@gmail.com>
2020-11-10 08:55:27 -0600

    sqrt

--- src/app/sqrt/sqrt.ts
+++ src/app/sqrt/sqrt.ts
@@ -1,6 +1,7 @@
-export function sqr(oper1){
-    let sqr = 0;
+export function sqrt(oper1) {  
+    // refactoring
+    let sqrt = 0;
 
-    sqr = Math.sqrt(oper1);
-    return sqr;
+    sqrt = Math.sqrt(oper1);
+    return (sqrt);
 }
